13c89362bcb36a808cef0a6cc725ea4f03cc6730
alitemel89 <alitemel89.ce@gmail.com>
2021-08-27 09:53:01 +0300

    install dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
+import { useState } from 'react';
+import { useQuery } from 'react-query';
 
-function App() {
+// Components
+import Drawer from '@material-ui/core/Drawer';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Drawer/>
     </div>
-  );
+  )
 }
 
 export default App;
